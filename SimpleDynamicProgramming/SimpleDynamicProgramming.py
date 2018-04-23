c4a1d077fba08ce2d759b75bd99d5d9334855d7e
Wes <wesleywyatttaylor@gmail.com>
2018-04-23 10:16:30 -0600

    Finished GetShortestDepth

--- SimpleDynamicProgramming/SimpleDynamicProgramming.py
+++ SimpleDynamicProgramming/SimpleDynamicProgramming.py
@@ -16,21 +16,34 @@ class TreeNode(object):
 def mkCh(amount, coins):
     #this algorithm will brute-force the problem to find all coin combinations, and then choose the smallest amount
     #create first level of tree
-    root = []
+    root = TreeNode(0)
     for i in range(0, len(coins), 1):
         if amount-coins[i]>=0:
-            root.append(TreeNode(coins[i]))
+            root.addChild(TreeNode(coins[i]))
     #call helper method to build tree of all possible payments from root
-    for i in range(0, len(root), 1):
-        mkChHelper(root[i], amount-root[i].getValue, coins)
+    for i in range(0, len(root.children), 1):
+        mkChHelper(root.children[i], amount-root.children[i].getValue, coins)
 
-    return change
+    return getShortestDepth(root)[0]
+
+def getShortestDepth(root):
+    if(root.children==[]):
+        return [[root.value], 1]
+    smallest = getShortestDepth(root.children[0])
+    for i in range(1, len(root.children),1):
+        current = getShortestDepth(root.children[i])
+        if(current[1]<smallest[1]):
+            smallest=current
+    smallest[0].append(root.value)
+    print(smallest[0])
+    return [smallest[0], smallest[1]+1]
 
 def mkChHelper(node, amount, coins):
     for i in range(0, len(coins), 1):
         if amount-coins[i]>=0:
             node.getChildren.append(TreeNode(coins[i]))
     return None
+
 if __name__ == "__main__":
     #coinset used
     coins = [1,5,10,25]
